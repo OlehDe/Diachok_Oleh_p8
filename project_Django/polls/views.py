7e14492d2f0bab2eff34ac3f01d23606df59b101
OlehDe <oleh.r.diachok@ukd.edu.ua>
2025-03-24 11:01:02 +0200

    пр9

--- project_Django/polls/views.py
+++ project_Django/polls/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Question
 
 # Create your views here.
+
+# from django.shortcuts import render
+# from .models import Question  # Переконайтеся, що модель імпортована
+
+def question_list(request):
+    questions = Question.objects.all()
+    context = {'questions': questions}
+    return render(request, 'question_list.html', context)
