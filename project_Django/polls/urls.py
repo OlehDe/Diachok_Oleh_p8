d887bcf1a70f4e24cd1b05bc9b8c6a3525bc5b11
OlehDe <oleh.r.diachok@ukd.edu.ua>
2025-04-07 10:51:47 +0300

    пр10.2

--- project_Django/polls/urls.py
+++ project_Django/polls/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
 from .views import register_view
+from django.urls import path
+from . import views
 
 app_name = 'polls'
 urlpatterns = [
     path('', views.question_list, name='question_list'),
     path('register/', register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout')
 ]
--- project_Django/polls/views.py
+++ project_Django/polls/views.py
@@ -2,12 +2,10 @@ from django.shortcuts import render
 from .models import Question
 
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from .forms import RegistrationForm
-# Create your views here.
+from django.contrib.auth import login, authenticate, logout
+from .forms import RegistrationForm, UserLoginForm
 
-# from django.shortcuts import render
-# from .models import Question  # Переконайтеся, що модель імпортована
+# Create your views here.
 
 def question_list(request):
     questions = Question.objects.all()
@@ -23,8 +21,27 @@ def register_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('http://127.0.0.1:8000/polls/')  # Змініть 'home' на вашу домашню сторінку
+            return redirect('http://127.0.0.1:8000/polls/')
     else:
         form = RegistrationForm()
 
     return render(request, 'register.html', {"form": form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('http://127.0.0.1:8000/polls/')
+    else:
+        form = UserLoginForm()
+    return render(request, 'login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
